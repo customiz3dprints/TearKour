bbee675b359e631650afe24541470a00c4c2196e
DDani6 <hestegimposztor2@gmail.com>
2026-04-28 22:59:17 +0200

    Initial mod files

---
+++ main.lua
@@ -0,0 +1,27 @@
+#version 2
+
+--This script will run on all levels when mod is active.
+--Modding documentation: http://teardowngame.com/modding
+--API reference: http://teardowngame.com/modding/api.html
+
+function server.init()
+end
+
+function server.tick(dt)
+end
+
+function server.update(dt)
+end
+
+function client.init()
+end
+
+function client.tick(dt)
+end
+
+function client.update(dt)
+end
+
+function client.draw(dt)
+end
+
